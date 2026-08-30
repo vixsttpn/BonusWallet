4a162a61743ae4112ba64392081e7509160f81fc
BonusWallet <mbgkzw@gmail.com>
2026-08-30 22:28:04 +0400

    Fix minSdk 26 for adaptive icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.bonuswallet.app"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
